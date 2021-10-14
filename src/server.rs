21a7fc041407fefa37b720ef7d292ea4512b9dd4
PrajwalCH <prajjwal2058@gmail.com>
2021-10-14 17:24:29 +0545

    feat: implement basic server::start() and handle_request function

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,8 @@
 pub mod map_route;
 
 use std::env;
-use std::net::Ipv4Addr;
+use std::io::{self, Read};
+use std::net::{Ipv4Addr, SocketAddr, TcpListener, TcpStream};
 use std::path::PathBuf;
 
 #[derive(Debug)]
@@ -19,4 +20,29 @@ impl Default for ServerConfig {
             path: env::current_dir().unwrap_or(PathBuf::from(".")),
         }
     }
+}
+
+pub fn start(server_config: ServerConfig) -> io::Result<()> {
+    let routes = map_route(&server_config.path)?;
+    let sock_addr = SocketAddr::from((server_config.host_addr, server_config.port_num));
+    let listener = TcpListener::bind(sock_addr)?;
+
+    for stream in listener.incoming() {
+        let stream = stream?;
+
+        println!("Connection established");
+        handle_request(stream);
+    }
+
+    println!("{:#?}", routes);
+    println!("{:?}", sock_addr);
+    Ok(())
+}
+
+fn handle_request(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("{}", String::from_utf8_lossy(&buffer[..]));
 }
