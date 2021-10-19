a59e63f605be4550945fcd107b8f902b7ac3d465
PrajwalCH <prajjwal2058@gmail.com>
2021-10-19 15:25:46 +0545

    refactor: change todo message for headers parser

--- src/server.rs
+++ src/server.rs
@@ -63,7 +63,7 @@ fn handle_connection(mut stream: TcpStream) {
 
 fn parse_request(buffer: &[u8]) -> HTTPRequest {
     let (request_line, _request_line_end) = parse_request_line(&buffer);
-    // TODO: implement headers parser
+    // TODO: implement headers parser if needed
     //let (_, headers) = buffer.split_at(request_line_end);
     println!("{:?}", request_line);
 
